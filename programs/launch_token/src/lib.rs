be93e53e3eed99171bdd23f1c88bae59bebafffb
A91y <65825207+A91y@users.noreply.github.com>
2025-03-23 17:46:29 +0530

    add launch module and integrate launch handler in lib.rs

--- programs/launch_token/src/lib.rs
+++ programs/launch_token/src/lib.rs
@@ -4,7 +4,7 @@ pub mod instructions;
 pub mod state;
 
 use anchor_lang::prelude::*;
-use instructions::initialize::*;
+use instructions::{initialize::*, launch::*};
 
 declare_id!("8fAeJ4DieZX4DRuyxS8hy3onnNrMo8zEFUp2dJF5MwTY");
 
@@ -15,6 +15,11 @@ pub mod launch_token {
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         initialize_handler(ctx)?;
+        Ok(())
+    }
+
+    pub fn launch_token(ctx: Context<Launch>, args: LaunchArgs) -> Result<()> {
+        launch_handler(ctx, args)?;
         Ok(())
     }
 }
